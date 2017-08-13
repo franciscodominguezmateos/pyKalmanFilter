72a8c521ab8009ad77bcfa52476a1f33dce26b06
Francisco Dominguez Mateos <francisco.dominguez@urjc.es>
2017-08-13 13:34:35 +0200

    pyEKFlineTest work fine

--- pyKamanFilter/src/pyEKFlineTest.py
+++ pyKamanFilter/src/pyEKFlineTest.py
@@ -18,11 +18,20 @@ def getGlobalLine(X,z):
     y=X[1,0]
     theta=X[2,0]
     aw=a+theta
-    aw=atan2(sin(aw),cos(aw))
-    rw=r+(x*cos(aw)+y*sin(aw))
-    zw=np.matrix([[rw],[aw]])
+    #Normalize angle
+    awn=atan2(sin(aw),cos(aw))
+    rw=r+(x*cos(awn)+y*sin(awn))
+    zw=np.matrix([[rw],[awn]])
     return zw
-def plotLine(z):
+def getCenterLine(z):
+    r=z[0,0]
+    alpha=z[1,0]
+
+    xr=r*cos(alpha)
+    yr=r*sin(alpha)
+    return xr,yr
+    
+def plotLine(z,color="r"):
     r=z[0,0]
     alpha=z[1,0]
     a=cos(alpha)
@@ -30,7 +39,7 @@ def plotLine(z):
     c=r
     xr=r*cos(alpha)
     yr=r*sin(alpha)
-    plt.plot([xr],[yr],'r+')
+    plt.plot([xr],[yr],color+'+')
     #if abs(alpha)<PI/2.0 :
     if b<1e-6:
         x0=np.sign(c)*10
@@ -53,7 +62,7 @@ def plotLine(z):
 #             x0=-b/a*y0+c/a
 #             y1=-10
 #             x1=-b/a*y1+c/a
-    plt.plot([x0,x1],[y0,y1],"r",alpha=0.5)
+    plt.plot([x0,x1],[y0,y1],color,alpha=0.5)
     
 if __name__ == '__main__':
     vpm=kf.VelocityProcessModel()
@@ -62,13 +71,13 @@ if __name__ == '__main__':
     u=np.matrix([1.0,0.11]).T
     lmm=kf.LineMeasurementModel()
     map=[]
-    map.append(np.array([ 10, 0.0]))
+    #map.append(np.array([ 10, 0.0]))
     map.append(np.array([ 10, PI/2.0]))
-    map.append(np.array([ 10, PI]))
+    #map.append(np.array([ 10, PI]))
     map.append(np.array([ 10,-PI/2.0]))
     map.append(np.array([ 10, PI/4.0]))
     map.append(np.array([ 10, PI*3.0/8.0]))
-    map.append(np.array([ -10, PI*1.0/8.0]))
+    #map.append(np.array([ -10, PI*1.0/8.0]))
     lmm.m=map
     cov=np.array([[  8.87790565,   4.65839419],
                   [  4.65839419,  12.01291835]]) #[[0.4, 9],[9, 10]])
@@ -76,7 +85,7 @@ if __name__ == '__main__':
                   [  0,  0.01]]) #[[0.4, 9],[9, 10]])
     for r,a in map:
         plotLine(np.matrix([[r],[a]]))
-    STEPS=60
+    STEPS=55
     for i in range(STEPS):
         print "At:"
         print X
@@ -108,13 +117,18 @@ if __name__ == '__main__':
             if np.linalg.det(Xcov)<0.002:
                 continue
             i=np.random.randint(0,len(map))
+            #i=j
             lmm.setC(i)#observe landmark 0
             zd=lmm.eval(X)
             #z=zd
             z=np.matrix(np.array([zd[0,0]+np.random.randn(1)[0]*0.01525,zd[1,0]+np.random.randn(1)[0]*0.01525])).T
-            x,y=getGlobalLine(X,z)
+            print zd
+            print z
+            zw=getGlobalLine(X,z)
+            plotLine(zw,"g")
             #plt.plot([x], [y], 'b*')
-            #plt.plot([x,X[0,0]],[y,X[1,0]],"g",alpha=0.2)
+            x,y=getCenterLine(zw)
+            plt.plot([x,X[0,0]],[y,X[1,0]],"g",alpha=0.2)
             X,Xcov=kalman.update(z)
             cp.plot_cov_ellipse(Xcov[:2,:2].copy(), X[:2,:].copy(), nstd=3, alpha=0.5, color='blue')
             kalman.X_=X
